79c2047a5b308f47fb57aaf0040351fe2e8fdf9e
Fsociety <fsociety@pop-os.localdomain>
2021-03-03 02:41:42 +0530

    QrCode implemented in registeration

--- src/frontend/digital_village/lib/Screens/Signup/components/body.dart
+++ src/frontend/digital_village/lib/Screens/Signup/components/body.dart
@@ -6,6 +6,7 @@ import '../../../components/rounded_input_field.dart';
 import '../../../components/already_have_an_account_acheck.dart';
 import '../../../components/rounded_password_field.dart';
 import '../../../constants.dart';
+import '../../../Screens/Signup/components/qrcode.dart';
 import '../../../components/floating_action_buttoncus.dart';
 import 'package:flutter_svg/svg.dart';
 
@@ -100,7 +101,16 @@ class Body extends StatelessWidget {
               // onPressed: Null,// On press per qrcode code wala karna
               child: Icon(Icons.qr_code),
               backgroundColor: kPrimaryColor,
-              // onPressed: ,
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return Qrcode();
+                    },
+                  ),
+                );
+              },
             ),
             );
   }
---
+++ src/frontend/digital_village/lib/Screens/Signup/components/qrcode.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:barcode_scan/barcode_scan.dart';
+import 'package:flutter/services.dart';
+
+class Qrcode extends StatefulWidget {
+  @override
+  _QrcodeState createState() => _QrcodeState();
+}
+
+class _QrcodeState extends State<Qrcode> {
+  String result = "Scan the QR code on your Aadhar card";
+
+  Future _scanQR() async {
+    try {
+      String qrResult = await BarcodeScanner.scan();
+      setState(() {
+        result = qrResult;
+      });
+    } on PlatformException catch (ex) {
+      if (ex.code == BarcodeScanner.CameraAccessDenied) {
+        setState(() {
+          result = "Camera permission was denied";
+        });
+      } else {
+        setState(() {
+          result = "Unknown Error $ex";
+        });
+      }
+    } on FormatException {
+      setState(() {
+        result = "You pressed the back button before scanning anything";
+      });
+    } catch (ex) {
+      setState(() {
+        result = "Unknown Error $ex";
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Aadhar Scan"),
+      ),
+      body: Center(
+        child: Text(
+          result,
+          style: new TextStyle(fontSize: 20),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        icon: Icon(Icons.camera_alt),
+        label: Text("Scan"),
+        onPressed: _scanQR,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
