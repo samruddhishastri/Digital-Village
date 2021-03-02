b026cdc1ea96b2f6b8f7bbf40b9253ff9f9d8b17
Fsociety <fsociety@pop-os.localdomain>
2021-03-03 01:44:37 +0530

    editted Dashboard

--- src/frontend/digital_village/lib/Screens/Dashboard/components/body.dart
+++ src/frontend/digital_village/lib/Screens/Dashboard/components/body.dart
@@ -16,72 +16,89 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Background(
-      child: Container(
-        margin: EdgeInsets.fromLTRB(0,20,0,0),
+        child: Row(children: <Widget>[
+      Container(
+        margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
         padding: EdgeInsets.all(1),
         child: Row(children: <Widget>[
-    
-        //icon with label below it
-        Container(
-          padding: EdgeInsets.all(15),
-          child: Column(
-            children: <Widget>[
-              Icon(
-                Icons.people_alt_outlined,
-                size: 40,
-                color: Colors.white,
-              ),
-              Text('Add Dependents\n'),
-              Icon(
-                Icons.face,
-                size: 40,
-                color: Colors.white,
-              ),
-              Text('Complaints\n'),
-            ],
+          //icon with label below it
+          Container(
+            padding: EdgeInsets.all(15),
+            child: Column(
+              children: <Widget>[
+                Icon(
+                  Icons.people_alt_outlined,
+                  size: 40,
+                  color: Colors.white,
+                ),
+                Text('Add Dependents\n', style: TextStyle(color: Colors.white)),
+                Icon(
+                  Icons.face,
+                  size: 40,
+                  color: Colors.white,
+                ),
+                Text('Complaints\n', style: TextStyle(color: Colors.white)),
+              ],
+            ),
           ),
-        ),
-        Container(
-          
-          padding: EdgeInsets.all(15),
-          child: Column(
-            children: <Widget>[
-              Icon(
-                Icons.file_copy_outlined,
-                size: 40,
-                color: Colors.white,
-              ),
-              Text('Organisation chart\n'),
-            
-              Icon(
-                Icons.download_rounded,
-                size: 40,
-                color: Colors.white,
-              ),
-              Text('Download forms\n'),
-            ],
+          Container(
+            padding: EdgeInsets.all(15),
+            child: Column(
+              children: <Widget>[
+                Icon(
+                  Icons.file_copy_outlined,
+                  size: 40,
+                  color: Colors.white,
+                ),
+                Text('Organisation chart\n',
+                    style: TextStyle(color: Colors.white)),
+                Icon(
+                  Icons.download_rounded,
+                  size: 40,
+                  color: Colors.white,
+                ),
+                Text('Download forms\n', style: TextStyle(color: Colors.white)),
+              ],
+            ),
           ),
-        ),
-        Container(
-          padding: EdgeInsets.all(15),
-          child: Column(
-            children: <Widget>[
-              Icon(
-                Icons.credit_card,
-                size: 40,
-                color: Colors.white,
-              ),
-              Text('Payments\n'),
-              Icon(
-                Icons.info,
-                size: 40,
-                color: Colors.white,
-              ),
-              Text('About us\n'),
-            ],
+          Container(
+            padding: EdgeInsets.all(15),
+            child: Column(
+              children: <Widget>[
+                Icon(
+                  Icons.credit_card,
+                  size: 40,
+                  color: Colors.white,
+                ),
+                Text('Payments\n', style: TextStyle(color: Colors.white)),
+                Icon(
+                  Icons.info,
+                  size: 40,
+                  color: Colors.white,
+                ),
+                Text('About us\n', style: TextStyle(color: Colors.white)),
+              ],
+            ),
           ),
-        )
-      ]),
-    ));
+        ]),
+      ),
+      // Positioned(
+      //   top: size.longestSide - 100,
+      //   right: size.width - 200,
+      //   child: Container(
+      //     // padding: EdgeInsets.all(15),
+      //     child: Column(
+      //       children: <Widget>[
+      //         Text('Add Dependents\n',style: TextStyle(color: Colors.white) ),
+      //         Icon(
+      //           Icons.face,
+      //           size: 40,
+      //           color: Colors.white,
+      //         ),
+      //         Text('Complaints\n',style: TextStyle(color: Colors.white)),
+      //       ],
+      //     ),
+      //   )),
+    ]));
   }
 }
