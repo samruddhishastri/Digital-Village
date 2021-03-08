0f86d45fcb4911d2ccf12c6dc96a03dd7088b92e
Fsociety <fsociety@pop-os.localdomain>
2021-03-08 16:26:27 +0530

    some changes and Flutter updated

--- src/frontend/digital_village/lib/main.dart
+++ src/frontend/digital_village/lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.orange,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: WelcomeScreen(),
+      home: WelcomeScreen(), 
     );
   }
 }
