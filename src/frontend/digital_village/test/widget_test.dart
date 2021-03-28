e77aac7fda6f1b9ce79b7bb1eea3e9cf0f927dfe
Fsociety <fsociety@pop-os.localdomain>
2021-03-28 18:31:01 +0530

    refixed older commit

--- src/frontend/digital_village/test/widget_test.dart
+++ src/frontend/digital_village/test/widget_test.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+<<<<<<< HEAD
+// <<<<<<< HEAD
+=======
 
+>>>>>>> e262a7f397a7632173f7dfa27e2472a03a969289
 import 'package:temp/main.dart';
+// =======
+// >>>>>>> d7c142d8dc64abdcf3257113d8058495fcd93ca6
+// import 'package:temp/main.dart';
 
 
 
